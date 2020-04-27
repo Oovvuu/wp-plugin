f459c2839aca642a65de7362bf05c2897996c07f
Kyle Benk <kjbenk@gmail.com>
2020-04-27 13:49:49 -0600

    Add comment

--- components/app/index.jsx
+++ components/app/index.jsx
@@ -3,6 +3,9 @@ import App from './app';
 const { PluginDocumentSettingPanel } = wp.editPost;
 const { registerPlugin } = wp.plugins;
 
+/**
+ * Adds a new Document Settings Panel for all Oovvuu video embedding functionality.
+ */
 const OovvuuPluginDocumentSettingPanel = () => (
   <PluginDocumentSettingPanel
     name="oovvuu"
