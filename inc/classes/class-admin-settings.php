730f93570851e2c1d7e0211ab73805182dec860c
Kyle Benk <kjbenk@gmail.com>
2020-04-29 12:48:10 -0600

    Add mock admin page

--- inc/classes/class-admin-settings.php
+++ inc/classes/class-admin-settings.php
@@ -32,24 +32,41 @@ class Admin_Settings {
 	}
 
 	/**
-	 * Options page setup.
+	 * Settings page setup.
 	 *
 	 * @since 1.0.0
 	 */
 	public function setup_options_page() {
-		add_submenu_page(
-			'oovvuu_settings',
-			__( 'Oovvuu Settings', 'oovvuu' ),
-			__( 'Oovvuu Settings', 'oovvuu' ),
+		add_menu_page(
+			__( 'Oovvuu', 'oovvuu' ),
+			__( 'Oovvuu', 'oovvuu' ),
 			/**
 			 * Filters the settings page capabilty.
 			 *
 			 * @param string The settings page capability.
 			 */
-			apply_filters( 'oovvuu_settings_capability', 'manage_options' ),
+			apply_filters('oovvuu_settings_capability', 'manage_options' ),
 			$this->page_name,
 			[ $this, 'page_options_render' ]
 		);
+	}
+
+	/**
+	 * Settings page render.
+	 *
+	 * @since 1.0.0
+	 */
+	public function page_options_render() {
+		/**
+		 * Filters the settings page capabilty.
+		 *
+		 * @param string The settings page capability.
+		 */
+		if ( ! current_user_can( apply_filters( 'oovvuu_settings_capability', 'manage_options' ) ) ) {
+			wp_die( esc_html__( 'You do not have permissions to access this page.', 'oovvuu' ) );
+		}
+
+		echo 'Settings...';
 	}
 }
 
