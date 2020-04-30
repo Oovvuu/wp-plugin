4918d3bc074858c5b8653e74d6c6be3e916e5881
Kyle Benk <kjbenk@gmail.com>
2020-04-30 10:50:04 -0600

    Add workflow to get access token

--- inc/classes/class-auth.php
+++ inc/classes/class-auth.php
@@ -36,9 +36,18 @@ class Auth {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @var \Auth0
+	 * @var \Auth0\SDK\Auth0
 	 */
 	private $client;
+
+	/**
+	 * The Auth0 Authentication client.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var \Auth0\SDK\API\Authentication
+	 */
+	private $authentication_client;
 
 	/**
 	 * The admin post action used for the redirect callback.
@@ -108,7 +117,7 @@ class Auth {
 			$client_args = [
 				'domain'        => $this->domain,
 				'client_id'     => $this->client_id,
-				// 'client_secret' => get_option( 'oovvuu_auth0_client_secret', '' ),
+				'client_secret' => get_option( 'oovvuu_auth0_client_secret', '' ),
 				'redirect_uri'  => $this->get_redirect_callback(),
 			];
 
@@ -124,12 +133,37 @@ class Auth {
 			$this->client = null;
 
 			if ( \method_exists( $exception, 'getMessage' ) ) {
-				$this->admin_notice_state   = 'error';
-				$this->admin_notice_message = __( 'Oovvuu: Unable to create Auth0 connection. Failed with error: ', 'oovvuu' ) . '<code>' . $exception->getMessage() . '</code>';
+				$this->set_admin_notice( 'error', __( 'Oovvuu: Unable to create Auth0 connection. Failed with error: ', 'oovvuu' ) . '<code>' . $exception->getMessage() . '</code>' );
 			}
 		}
 
 		return $this->client;
+	}
+
+	/**
+	 * Get the Auth0 Authentication client to connect with the API.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return \Auth0\SDK\API\Authentication|null The proper Auth0 object or null.
+	 */
+	public function get_authentication_client() {
+		// Authentication call not found.
+		if ( ! class_exists( '\Auth0\SDK\API\Authentication' ) ) {
+			return null;
+		}
+
+		// Client is already created.
+		if ( $this->authentication_client instanceof \Auth0\SDK\API\Authentication ) {
+			return $this->authentication_client;
+		}
+
+		$this->authentication_client = new \Auth0\SDK\API\Authentication(
+			$this->domain,
+			$this->client_id
+		);
+
+		return $this->authentication_client;
 	}
 
 	/**
@@ -138,19 +172,24 @@ class Auth {
 	 * @since 1.0.0
 	 */
 	public function show_admin_notices() {
-		if ( empty( $this->admin_notice_state ) || empty( $this->admin_notice_message ) ) {
+		$current_admin_notice = $this->get_admin_notice();
+
+		if ( empty( $current_admin_notice['type'] ) || empty( $current_admin_notice['message'] ) ) {
 			return;
 		}
 
 		?>
-		<div class="notice notice-<?php echo esc_attr( $this->admin_notice_state ); ?>">
+		<div class="notice notice-<?php echo esc_attr( $current_admin_notice['type'] ); ?>">
 			<p>
 				<?php
-				echo wp_kses_post( $this->admin_notice_message );
+				echo wp_kses_post( $current_admin_notice['message'] );
 				?>
 			</p>
 		</div>
 		<?php
+
+		// Clear the current notice.
+		$this->clear_admin_notice();
 	}
 
 	/**
@@ -164,17 +203,17 @@ class Auth {
 
 		if ( isset( $_GET['code'] ) ) {
 
-			// Successful authentication.
-			wp_safe_redirect( esc_url_raw( add_query_arg( 'msg', 'success', $redirect_url ) ) );
-			exit;
+			// Code exchange.
+			$this->code_exchange( sanitize_text_field( wp_unslash( $_GET['code'] ) ) );
 		} elseif ( isset( $_GET['error'] ) ) {
 
 			// Unable to authenticate.
-			wp_safe_redirect( esc_url_raw( $redirect_url ) );
-			exit;
+			// wp_safe_redirect( esc_url_raw( $redirect_url ) );
+			// exit;
 		}
 
-		echo 'Test';
+		wp_safe_redirect( esc_url_raw( $this->get_profile_url() ) );
+		exit;
 	}
 
 	/**
@@ -198,31 +237,109 @@ class Auth {
 	 * @return string The authorization link.
 	 */
 	public function get_auth_link() {
-		$client = $this->get_client();
+		$authentication_client = $this->get_authentication_client();
 
 		// Invalid client.
-		if ( empty( $client ) ) {
+		if ( empty( $authentication_client ) ) {
 			return '';
 		}
 
-		$authentication = new \Auth0\SDK\API\Authentication(
-			$this->domain,
-			$this->client_id
-		);
-
-		return $authentication->get_authorize_link(
+		return $authentication_client->get_authorize_link(
 			'code',
 			$this->get_redirect_callback()
 		);
 	}
 
 	/**
+	 * Performs a request to get an access token via an authorization code.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $code Authorization code.
+	 */
+	public function code_exchange( $code ) {
+		// Invalid code.
+		if ( empty( $code ) || ! is_string( $code ) ) {
+			return;
+		}
+
+		$authentication_client = $this->get_authentication_client();
+
+		// Invalid client.
+		if ( empty( $authentication_client ) ) {
+			return;
+		}
+
+		try {
+			// Perform the exchange.
+			$authentication_client->code_exchange( $code, $this->get_redirect_callback() );
+		} catch ( \Exception $exception ) {
+			if ( \method_exists( $exception, 'getMessage' ) ) {
+				$this->set_admin_notice( 'error', __( 'Oovvuu: Unable to create Auth0 connection. Failed with error: ', 'oovvuu' ) . '<code>' . $exception->getMessage() . '</code>' );
+			}
+		}
+
+		wp_safe_redirect( esc_url_raw( $this->get_profile_url() ) );
+		exit;
+	}
+
+	/**
+	 * Set admin notices.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $type    The notice type (error, success).
+	 * @param string $message The notice message.
+	 */
+	public function set_admin_notice( $type, $message ) {
+		update_option( 'oovvuu_auth0_admin_notice_type', $type );
+		update_option( 'oovvuu_auth0_admin_notice_message', $message );
+	}
+
+	/**
+	 * Get the current admin notice.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return array The admin notice properties.
+	 */
+	public function get_admin_notice() {
+		return [
+			'type'    => get_option( 'oovvuu_auth0_admin_notice_type' ),
+			'message' => get_option( 'oovvuu_auth0_admin_notice_message' ),
+		];
+	}
+
+	/**
+	 * Clear the current admin notice.
+	 *
+	 * @since 1.0.0
+	 */
+	public function clear_admin_notice() {
+		delete_option( 'oovvuu_auth0_admin_notice_type' );
+		delete_option( 'oovvuu_auth0_admin_notice_message' );
+	}
+
+	/**
 	 * Returns the redirect callback URL.
+	 *
+	 * @since 1.0.0
 	 *
 	 * @return string The redirect callback.
 	 */
 	public function get_redirect_callback() {
 		return admin_url( '/admin-post.php?action=' . $this->redirect_callback_action );
+	}
+
+	/**
+	 * Returns the user profile URL.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return string The redirect callback.
+	 */
+	public function get_profile_url() {
+		return admin_url( '/profile.php' );
 	}
 }
 
