a7ccb0de503322fa01e6bb0f9fa93a9d3f96327a
Ryan Domingue <rychdom+github@gmail.com>
2020-05-07 14:48:59 -0700

    OVU-62 Moves assets & utils to client directory; uses aliases

--- components/appClassic/index.jsx
+++ components/appClassic/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from '../app/app';
+import App from 'components/app/app';
 
 render(<App />, document.getElementById('oovvuu-classic-editor-react-app'));
