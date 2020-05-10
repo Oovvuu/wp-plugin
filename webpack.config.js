98232e894070f75c45a2b4fcb81ca7c3739350dd
Ryan Domingue <rychdom+github@gmail.com>
2020-05-10 12:18:24 -0700

    OVU-62 Moves global styles to directory for better modules exclusion

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,7 @@ module.exports = (env, argv) => {
                 modules: {
                   mode: (resourcePath) => {
                     // Exclude core styles from this loader's CSS Modules mode.
-                    if (/client\/scss/i.test(resourcePath)) {
+                    if (/client\/scss\/global/i.test(resourcePath)) {
                       return 'global';
                     }
 
