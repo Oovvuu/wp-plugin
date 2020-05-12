367d9721d448933811d71ae4fbef0b7b723c1d11
Robert Pratt <robert@alley.co>
2020-05-12 13:21:35 -0500

    Adds scss to moduleNameMapper.

--- config/jest.js
+++ config/jest.js
@@ -4,8 +4,9 @@ module.exports = {
   moduleNameMapper: {
     // eslint-disable-next-line max-len
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/mocks/fileMock.js',
-    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
   },
+  modulePaths: ['<rootDir>'],
   rootDir: '..',
   setupFiles: [],
   setupFilesAfterEnv: [
