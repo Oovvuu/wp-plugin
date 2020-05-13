9ce7216c0ce69917de3a7f54f42ef5da8b5a910f
Robert Pratt <robert@alley.co>
2020-05-12 20:06:41 -0500

    Update tests to finalize WIP.

--- components/keywordSelector/keywordSelector.spec.jsx
+++ components/keywordSelector/keywordSelector.spec.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import KeywordSelector from './keywordSelector';
 import GeneratedList from './generatedList';
+import UserList from './userList';
 import KeywordItem from './keywordItem';
 
 const mockKeywordList = [];
-const mockPostTitle = 'Post Title';
-const onFetchVideosFn = jest.fn();
-const onUpdateFn = jest.fn();
 
 describe('KeywordSelector', () => {
-  it('Renders KeywordList', () => {
+  it('Renders GeneratedList', () => {
     const wrapper = shallow(<KeywordSelector
       keywords={mockKeywordList}
-      onFetchVideos={onFetchVideosFn}
-      onUpdateKeywords={onUpdateFn}
-      postTitle={mockPostTitle}
+      onKeywordsUpdated={jest.fn()}
     />);
 
     expect(wrapper.find(GeneratedList)).toHaveLength(1);
   });
 
-  it.skip('Adds selected KeywordItem correctly onUpdate()', () => {
-    // const updateFn = jest.fn();
-    // const wrapper = shallow(<KeywordSelector
-    //   keywords={mockKeywordList}
-    //   onFetchVideos={onFetchVideosFn}
-    //   onUpdateKeywords={onUpdateFn}
-    //   postTitle={mockPostTitle}
-    // />);
+  it('Renders UserList', () => {
+    const wrapper = shallow(<KeywordSelector
+      keywords={mockKeywordList}
+      onKeywordsUpdated={jest.fn()}
+    />);
+
+    expect(wrapper.find(UserList)).toHaveLength(1);
+  });
+
+  it('Updates selected items from the GeneratedList correctly', () => {
+    const onKeywordsUpdatedFn = jest.fn();
+    const wrapper = mount(<KeywordSelector
+      keywords={['keyword']}
+      onKeywordsUpdated={onKeywordsUpdatedFn}
+    />);
+
+    // Make sure we have one and only one KeywordItem.
+    expect(wrapper.find(KeywordItem)).toHaveLength(1);
+
+    // Select our generated keyword and verify it is added to the selections list.
+    wrapper.find('input[type="checkbox"]').at(0).simulate('change');
+    expect(onKeywordsUpdatedFn.mock.calls[0][0][0]).toContain('keyword');
+
+    // Deselect our generated keyword and verify it is removed from the selections list.
+    wrapper.find('input[type="checkbox"]').at(0).simulate('change');
+    expect(onKeywordsUpdatedFn.mock.calls[1][0]).toHaveLength(0);
   });
 });
