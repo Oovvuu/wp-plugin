9f9f839f0b486d87e0364d15690ffef5b12a2c4a
Robert Pratt <robert@alley.co>
2020-05-14 09:56:26 -0500

    Initial commit: Scaffold video player components, with smoke tests.

---
+++ components/actionButtons/remove.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ActionButtonWrapper from './index';
+
+const Remove = () => (
+  <ActionButtonWrapper>
+    <span>X</span>
+  </ActionButtonWrapper>
+);
+
+export default Remove;
