32cc7a6dc793085d011f881e70df8d17e5606d5a
Robert Pratt <robert@alley.co>
2020-05-14 07:17:43 -0500

    Refactor for useContext and to trigger fetches in-component (rather than via callback).

--- components/keywordPanel/keywordPanel.jsx
+++ components/keywordPanel/keywordPanel.jsx
@@ -1,31 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
+import oovvuuData from 'components/app/oovvuuDataContext';
+import getKeywords from 'services/getKeywords';
+import getVideos from 'services/getVideos';
+import getPostAttribute from 'services/getPostAttribute';
 import KeywordSelector from './keywordSelector';
 import styles from './keywordPanel.scss';
 
 const KeywordPanel = (props) => {
+  const { onSetKeywords } = props;
   const {
-    keywords, onFetchKeywords, onFetchVideos, onSetKeywords,
-  } = props;
+    dispatch,
+    state: { recommendedKeywords, selectedKeywords },
+  } = React.useContext(oovvuuData);
+  const id = getPostAttribute('id');
+
+  const handleFetchKeywords = async () => {
+    const title = getPostAttribute('title');
+    const content = getPostAttribute('content');
+    const response = await getKeywords(title, content, id);
+
+    if (!response.hasError) {
+      const { keywords } = response.data;
+      dispatch({ payload: keywords, type: 'UPDATE_RECOMMENDED_KEYWORDS' });
+    }
+  };
+
+  const handleFetchVideos = async () => {
+    const response = await getVideos(selectedKeywords, id);
+
+    if (!response.hasError) {
+      const { videos } = response.data;
+      dispatch({ payload: videos, type: 'UPDATE_RECOMMENDED_VIDEOS' });
+    }
+  };
 
   return (
     <div className={styles.panel}>
       <h3>{__('Recommended Keywords', 'oovvuu')}</h3>
       <KeywordSelector
-        keywords={keywords}
+        keywords={recommendedKeywords}
         onKeywordsUpdated={(updated) => onSetKeywords(updated)}
       />
       <p>
         <button
           type="button"
-          onClick={onFetchKeywords}
+          onClick={handleFetchKeywords}
         >
           {__('Get new Keywords', 'oovvuu')}
         </button>
         <button
           type="button"
-          onClick={onFetchVideos}
+          onClick={handleFetchVideos}
         >
           {__('Fetch Videos', 'oovvuu')}
         </button>
@@ -34,11 +61,6 @@ const KeywordPanel = (props) => {
   );
 };
 
-KeywordPanel.propTypes = {
-  keywords: PropTypes.arrayOf(PropTypes.string).isRequired,
-  onFetchKeywords: PropTypes.func.isRequired,
-  onFetchVideos: PropTypes.func.isRequired,
-  onSetKeywords: PropTypes.func.isRequired,
-};
+KeywordPanel.propTypes = { onSetKeywords: PropTypes.func.isRequired };
 
 export default KeywordPanel;
