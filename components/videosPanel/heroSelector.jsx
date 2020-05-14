77e128ec4d9dcef72f9f6c667b27fad8d663984a
Robert Pratt <robert@alley.co>
2020-05-14 09:58:44 -0500

    Extracts VideoCardWrapper component.

--- components/videosPanel/heroSelector.jsx
+++ components/videosPanel/heroSelector.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-// import PropTypes from 'prop-types';
 import OovvuuData from 'components/app/oovvuuDataContext';
+import PositionWrapper from 'components/position';
+import PositionToggle from 'components/positionToggle';
+import VideoCardWrapper from 'components/videoCard';
 
 const VideosPanel = () => {
   const { state: { recommendedVideos: { hero } } } = React.useContext(OovvuuData);
 
   return (
-    <>
-      <h3>{__('Hero', 'oovvuu')}</h3>
+    <PositionWrapper>
+      <header>
+        <h3>{__('Hero', 'oovvuu')}</h3>
+        <PositionToggle />
+      </header>
       <div>
-        {hero.map((value) => (
-          <div
-            key={value.id}
-          >
-            <p>{value.title}</p>
-            <img src={value.thumbnail.url} alt="" />
-          </div>
+        {hero.map(({ id, thumbnail, title }) => (
+          <VideoCardWrapper
+            id={id}
+            thumbnail={thumbnail}
+            title={title}
+          />
         ))}
       </div>
-    </>
+    </PositionWrapper>
   );
 };
 
--- components/videosPanel/positionTwoSelector.jsx
+++ components/videosPanel/positionTwoSelector.jsx
@@ -2,24 +2,29 @@ import React from 'react';
 import { __ } from '@wordpress/i18n';
 // import PropTypes from 'prop-types';
 import OovvuuData from 'components/app/oovvuuDataContext';
+import PositionWrapper from 'components/position';
+import PositionToggle from 'components/positionToggle';
+import VideoCardWrapper from 'components/videoCard';
 
 const PositionTwoSelector = () => {
   const { state: { recommendedVideos: { positionTwo } } } = React.useContext(OovvuuData);
 
   return (
-    <>
-      <h3>{__('4th Paragraph', 'oovvuu')}</h3>
+    <PositionWrapper>
+      <header>
+        <h3>{__('4th Paragraph', 'oovvuu')}</h3>
+        <PositionToggle />
+      </header>
       <div>
-        {positionTwo.map((value) => (
-          <div
-            key={value.id}
-          >
-            <p>{value.title}</p>
-            <img src={value.thumbnail.url} alt="" />
-          </div>
+        {positionTwo.map(({ id, thumbnail, title }) => (
+          <VideoCardWrapper
+            id={id}
+            thumbnail={thumbnail}
+            title={title}
+          />
         ))}
       </div>
-    </>
+    </PositionWrapper>
   );
 };
 
