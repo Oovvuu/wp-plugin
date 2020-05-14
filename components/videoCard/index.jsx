879e5eda5fdd99b6e135e75deda40feeba0f6992
Robert Pratt <robert@alley.co>
2020-05-14 09:59:41 -0500

    Stub in basic markup.

--- components/videoCard/index.jsx
+++ components/videoCard/index.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const VideoCardWrapper = () => <p>Hello, world</p>;
+const VideoCardWrapper = (props) => {
+  const { id, title, thumbnail: { url } } = props;
+
+  return (
+    <div
+      key={id}
+    >
+      <p>{title}</p>
+      <img src={url} alt="" />
+    </div>
+  );
+};
+
+VideoCardWrapper.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.shape({ url: PropTypes.string.isRequired }).isRequired,
+};
 
 export default VideoCardWrapper;
