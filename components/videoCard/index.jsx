8cc220bfa66c4959da0738d4e2ba0ee9d496a767
Kyle Benk <kjbenk@gmail.com>
2020-05-15 09:32:50 -0600

    Stub out brightcove video player

--- components/videoCard/index.jsx
+++ components/videoCard/index.jsx
@@ -1,18 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ReactPlayerLoader from '@brightcove/react-player-loader';
 
 /**
  * Stub component for the video card wrapper.
  */
 const VideoCardWrapper = (props) => {
-  const { id, title, thumbnail: { url } } = props;
+  const {
+    id, title, thumbnail: { url }, preview,
+  } = props;
+
+  const onSuccess = function (success) {
+    console.log(success);
+  };
+
+  const onFailure = function (failure) {
+    console.log(failure);
+  };
 
   return (
     <div
       key={id}
     >
       <p>{title}</p>
-      <img src={url} alt="" />
+      {
+        preview
+        && preview.brightcoveAccountId
+        && preview.brightcovePlayerId
+        && preview.brightcoveVideoId
+          ? (
+            <ReactPlayerLoader
+              accountId={preview.brightcoveAccountId}
+              playerId={preview.brightcovePlayerId}
+              videoId={preview.brightcoveVideoId}
+              onSuccess={(success) => { onSuccess(success); }}
+              onFailure={(failure) => { onFailure(failure); }}
+            />
+          )
+          : (<img src={url} alt="" />)
+      }
     </div>
   );
 };
@@ -21,6 +47,11 @@ VideoCardWrapper.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   thumbnail: PropTypes.shape({ url: PropTypes.string.isRequired }).isRequired,
+  preview: PropTypes.shape({
+    brightcoveVideoId: PropTypes.string,
+    brightcovePlayerId: PropTypes.string,
+    brightcoveAccountId: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default VideoCardWrapper;
