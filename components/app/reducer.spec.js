f4d4eb8e53469753293b16fd089b4618a602cfe6
Robert Pratt <robert@alley.co>
2020-05-15 15:59:29 -0500

    Update case after refactor.

--- components/app/reducer.spec.js
+++ components/app/reducer.spec.js
@@ -2,14 +2,14 @@ import initialState from './initialState';
 import reducer from './reducer';
 
 describe('Application reducer', () => {
-  it("Has case for 'UPDATE_SELECTED_HERO'", () => {
+  it("Has case for 'UPDATE_SELECTED_VIDEOS'", () => {
     const action = {
-      payload: { id: 'hero' },
-      type: 'UPDATE_SELECTED_HERO',
+      payload: { hero: { id: 'hero' } },
+      type: 'UPDATE_SELECTED_VIDEOS',
     };
 
     const newState = reducer(initialState, action);
 
-    expect(newState.selectedVideos.hero).toEqual(expect.objectContaining(action.payload));
+    expect(newState.selectedVideos.hero).toEqual(expect.objectContaining(action.payload.hero));
   });
 });
