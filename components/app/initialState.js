6770160cc337a0ba345df0ffb3e2822e5606388f
Robert Pratt <robert@alley.co>
2020-05-14 19:31:42 -0500

    Updates JSDoc. Fills out app state.

---
+++ components/app/initialState.js
@@ -0,0 +1,20 @@
+/**
+ * Passed as defaultValue to a React.Context instance and further into a useReducer instance
+ *   to define the state for a Redux-like application store.
+ *
+ * @type {{object}}
+ */
+const initialState = {
+  recommendedKeywords: [],
+  recommendedVideos: {
+    hero: [],
+    heroEmptyReason: null,
+    heroSecondary: [],
+    positionTwo: [],
+    positionTwoEmptyReason: null,
+    positionTwoSecondary: [],
+  },
+  selectedKeywords: [],
+};
+
+export default initialState;
