8c0914d9bfe022cb8bd2997c56c42ee8b96e04df
Ryan Domingue <rychdom+github@gmail.com>
2020-05-15 15:28:32 -0700

    OVU-14 Add the ThemeButton component

--- components/actionButtons/index.jsx
+++ components/actionButtons/index.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import buttons from 'shared/buttons.scss';
 
 /**
  * Wrapper component to create an icon button intended to trigger an action,
  *   such as closing a dialog.
  */
 const ActionButtonWrapper = (props) => {
-  const { children } = props;
+  const { children, style, disabled } = props;
 
-  return <button type="button">{children}</button>;
+  return (
+    <button
+      className={style}
+      type="button"
+      disabled={disabled}
+    >
+      <span>{children}</span>
+    </button>
+  );
+};
+
+ActionButtonWrapper.defaultProps = {
+  style: buttons.button,
+  disabled: false,
 };
 
 ActionButtonWrapper.propTypes = {
@@ -16,6 +30,8 @@ ActionButtonWrapper.propTypes = {
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
+  style: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
 export default ActionButtonWrapper;
