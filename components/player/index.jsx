68b3b69453b01fa3696b6eed23de8a5d3c8b531b
Kyle Benk <kjbenk@gmail.com>
2020-05-15 10:03:14 -0600

    Add logic to player

--- components/player/index.jsx
+++ components/player/index.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ReactPlayerLoader from '@brightcove/react-player-loader';
 
 /**
  * Stub component for the video player wrapper.
  */
-const PlayerWrapper = () => <p>Hello, world</p>;
+const PlayerWrapper = (props) => {
+  const { thumbnailUrl, preview } = props;
+
+  const onSuccess = function (success) {
+    console.log(success);
+  };
+
+  const onFailure = function (failure) {
+    console.log(failure);
+  };
+
+  return (
+    preview
+    && preview.brightcoveAccountId
+    && preview.brightcovePlayerId
+    && preview.brightcoveVideoId
+      ? (
+        <ReactPlayerLoader
+          accountId={preview.brightcoveAccountId}
+          playerId={preview.brightcovePlayerId}
+          videoId={preview.brightcoveVideoId}
+          onSuccess={(success) => { onSuccess(success); }}
+          onFailure={(failure) => { onFailure(failure); }}
+        />
+      )
+      : (<img src={thumbnailUrl} alt="" />)
+  );
+};
+
+PlayerWrapper.propTypes = {
+  thumbnailUrl: PropTypes.string.isRequired,
+  preview: PropTypes.shape({
+    brightcoveVideoId: PropTypes.string,
+    brightcovePlayerId: PropTypes.string,
+    brightcoveAccountId: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default PlayerWrapper;
--- components/videoCard/index.jsx
+++ components/videoCard/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactPlayerLoader from '@brightcove/react-player-loader';
+import PlayerWrapper from 'components/player';
 
 /**
  * Stub component for the video card wrapper.
@@ -10,35 +10,15 @@ const VideoCardWrapper = (props) => {
     id, title, thumbnail: { url }, preview,
   } = props;
 
-  const onSuccess = function (success) {
-    console.log(success);
-  };
-
-  const onFailure = function (failure) {
-    console.log(failure);
-  };
-
   return (
     <div
       key={id}
     >
       <p>{title}</p>
-      {
-        preview
-        && preview.brightcoveAccountId
-        && preview.brightcovePlayerId
-        && preview.brightcoveVideoId
-          ? (
-            <ReactPlayerLoader
-              accountId={preview.brightcoveAccountId}
-              playerId={preview.brightcovePlayerId}
-              videoId={preview.brightcoveVideoId}
-              onSuccess={(success) => { onSuccess(success); }}
-              onFailure={(failure) => { onFailure(failure); }}
-            />
-          )
-          : (<img src={url} alt="" />)
-      }
+      <PlayerWrapper
+        thumbnailUrl={url}
+        preview={preview}
+      />
     </div>
   );
 };
