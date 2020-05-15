cddd433fd9455a989fccef504d0c21f31771df52
Ryan Domingue <rychdom+github@gmail.com>
2020-05-15 14:13:41 -0700

    OVU-62 Corrects entry name (whoops!)

--- config/getEntries.js
+++ config/getEntries.js
@@ -23,7 +23,7 @@ module.exports = function getEntries(mode, entries) {
 
     return {
       ...acc,
-      [entry]: [...hotEntries, ...entry],
+      [entryKey]: [...hotEntries, ...entry],
     };
   }, {});
 };
