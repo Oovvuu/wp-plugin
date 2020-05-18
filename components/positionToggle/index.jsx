3a948ed75e34502941b6f6a0734fd0e04734f78c
Robert Pratt <robert@alley.co>
2020-05-18 09:13:38 -0500

    Adds markup and styles reference from Ryan's CodePen.

--- components/positionToggle/index.jsx
+++ components/positionToggle/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import styles from './positionToggle.scss';
 
 /**
  * Stub component for position toggle wrapper component.
  */
-const PositionToggleWrapper = () => <p>Hello, world</p>;
+const PositionToggleWrapper = () => (
+  <span className={styles.toggle}>
+    <label
+      htmlFor="toggle-1"
+      className="uppercase"
+    >
+      Show on article
+      <input
+        className=""
+        name="tag"
+        id="toggle-1"
+        value="Show on article"
+        type="checkbox"
+      />
+    </label>
+  </span>
+);
 
 export default PositionToggleWrapper;
