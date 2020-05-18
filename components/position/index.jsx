37548fcf2a5f5a6d293bdc4d9068d53b3babc1cb
Robert Pratt <robert@alley.co>
2020-05-18 12:57:07 -0500

    Adds header, content markup.

--- components/position/index.jsx
+++ components/position/index.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PositionToggleWrapper from 'components/positionToggle';
+import styles from './position.scss';
 
 /**
  * Stub component for the video position wrapper.
  */
 const PositionWrapper = (props) => {
   const { i18n: { __ } } = wp;
-  const { children } = props;
+  const { children, label } = props;
 
   return (
-    <>
-      <h3>{__('Position:', 'oovvuu')}</h3>
-      <PositionToggleWrapper />
-      { children }
-    </>
+    <div className={styles.wrapper}>
+      <header>
+        <h3 className={styles.title}>
+          <span>{__('Position:', 'oovvuu')}</span>
+          {` ${label}`}
+        </h3>
+        <PositionToggleWrapper />
+      </header>
+      <div className={styles.content}>
+        { children }
+      </div>
+    </div>
   );
 };
+
+PositionWrapper.defaultProps = { label: '' };
 
 PositionWrapper.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
+  label: PropTypes.string,
 };
 
 export default PositionWrapper;
