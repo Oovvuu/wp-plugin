993fe21cddb9bae0a0aa36274df8ea73351befaa
Robert Pratt <robert@alley.co>
2020-05-19 09:03:02 -0500

    Clean up failures, prop-types errors.

--- components/positionsPanel/videoCard/index.spec.jsx
+++ components/positionsPanel/videoCard/index.spec.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import VideoCardWrapper from './index';
-// import styles from './videoCard.scss';
 
-const mockVideo = { title: 'title', description: 'description', collection: { provider: { logo: { url: 'url' } } } };
-// jest.mock('./videoCard.scss');
-// styles.mockImplementation(() => ({ content: 'content', description: 'description' }));
+const mockVideo = {
+  collection: {
+    provider: {
+      logo: {
+        url: 'url',
+      },
+    },
+  },
+  description: 'description',
+  title: 'title',
+};
 
 describe('VideoCardWrapper', () => {
   it('Renders logo image', () => {
@@ -18,7 +25,7 @@ describe('VideoCardWrapper', () => {
 
   it('Renders header', () => {
     const wrapper = shallow(
-      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+      <VideoCardWrapper video={mockVideo} />,
     );
 
     expect(wrapper.find('header')).toHaveLength(1);
@@ -26,23 +33,23 @@ describe('VideoCardWrapper', () => {
 
   it('Renders video meta', () => {
     const wrapper = shallow(
-      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+      <VideoCardWrapper video={mockVideo} />,
     );
 
     expect(wrapper.find('div.meta')).toHaveLength(1);
   });
 
-  it.skip('Renders video content', () => {
+  it('Renders video content', () => {
     const wrapper = shallow(
-      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+      <VideoCardWrapper video={mockVideo} />,
     );
 
     expect(wrapper.find('div.content')).toHaveLength(1);
   });
 
-  it.skip('Renders video description', () => {
+  it('Renders video description', () => {
     const wrapper = shallow(
-      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+      <VideoCardWrapper video={mockVideo} />,
     );
 
     expect(wrapper.find('p.description')).toHaveLength(1);
