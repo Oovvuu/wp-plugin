453ec01573a5c2d4f93a1b9f46ae310c175c579a
Julio Braña Bouza <julio@alley.co>
2020-05-19 16:21:18 +0300

    videocard component + badge wip

--- components/positionsPanel/videoCard/index.jsx
+++ components/positionsPanel/videoCard/index.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './videoCard.scss';
+import Badge from './badge';
 
 /**
  * Displays an individual video with an position.
  */
 const VideoCardWrapper = (props) => {
-  const { positionKey, video: { title } } = props;
+  const {
+    positionKey,
+    video: { title, description, collection: { provider: { logo: { url } } } },
+  } = props;
 
   // TODO: Remove when positionKey is used.
   console.log(positionKey);
 
   return (
-    <div>
-      <p>{title}</p>
+    <div className={styles.wrapper}>
+      <div role="img" aria-label="logo">
+        <img src={url} alt="" />
+      </div>
+      <div className={styles.content}>
+        <header>
+          <h4>{title}</h4>
+          <Badge text="hello" />
+        </header>
+        <p className={styles.description}>{description}</p>
+      </div>
     </div>
   );
 };
@@ -21,6 +35,14 @@ VideoCardWrapper.propTypes = {
   positionKey: PropTypes.string.isRequired,
   video: PropTypes.shape({
     title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    collection: PropTypes.shape({
+      provider: PropTypes.shape({
+        logo: PropTypes.shape({
+          url: PropTypes.string.isRequired,
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
   }).isRequired,
 };
 
--- components/positionsPanel/videoCard/index.spec.jsx
+++ components/positionsPanel/videoCard/index.spec.jsx
@@ -1,15 +1,50 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import VideoCardWrapper from './index';
+// import styles from './videoCard.scss';
 
-const mockVideo = { title: 'title' };
+const mockVideo = { title: 'title', description: 'description', collection: { provider: { logo: { url: 'url' } } } };
+// jest.mock('./videoCard.scss');
+// styles.mockImplementation(() => ({ content: 'content', description: 'description' }));
 
 describe('VideoCardWrapper', () => {
-  it('Renders p', () => {
+  it('Renders logo image', () => {
     const wrapper = shallow(
       <VideoCardWrapper positionKey="hero" video={mockVideo} />,
     );
 
-    expect(wrapper.find('p')).toHaveLength(1);
+    expect(wrapper.find('div[role="img"][aria-label="logo"]')).toHaveLength(1);
+  });
+
+  it('Renders header', () => {
+    const wrapper = shallow(
+      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+    );
+
+    expect(wrapper.find('header')).toHaveLength(1);
+  });
+
+  it('Renders video meta', () => {
+    const wrapper = shallow(
+      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+    );
+
+    expect(wrapper.find('div.meta')).toHaveLength(1);
+  });
+
+  it.skip('Renders video content', () => {
+    const wrapper = shallow(
+      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+    );
+
+    expect(wrapper.find('div.content')).toHaveLength(1);
+  });
+
+  it.skip('Renders video description', () => {
+    const wrapper = shallow(
+      <VideoCardWrapper positionKey="hero" video={mockVideo} />,
+    );
+
+    expect(wrapper.find('p.description')).toHaveLength(1);
   });
 });
