8952f461e2c9009185345ce568215e2025827f48
Robert Pratt <robert@alley.co>
2020-05-21 20:34:43 -0500

    Update stubbing new item and handling for mutation and updating.

--- components/keywordPanel/keywordSelector/userKeywordList/index.jsx
+++ components/keywordPanel/keywordSelector/userKeywordList/index.jsx
@@ -1,32 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import uuid from 'react-uuid';
 import AddIcon from 'assets/add.svg';
 import ActionButton from 'components/actionButton';
 import KeywordList from '../keywordList';
 import styles from './userKeywordList.scss';
 
 /**
- * Stub component for the user-supplied list of keywords.
+ * Manages adding and deleting user-defined keywords. Wraps the KeywordList component
+ *   and passes through the onRemove() callback to enable editable functionality.
  */
 const UserList = (props) => {
   const { keywordItems, onMutate, onUpdate } = props;
 
+  // Need to keep a separate list to handle stubbing in a user-defined keyword.
+  const [selfKeywordItems, setSelfKeywordItems] = React.useState(keywordItems);
+
+  /**
+   * Stubs in a user-defined keyword item for input.
+   */
   const handleAdd = () => {
-    onMutate(
-      'add',
-      {
-        id: uuid(),
-        isSelected: true,
-        keyword: '',
-        type: 'user',
+    setSelfKeywordItems({
+      ...selfKeywordItems,
+      ...{
+        STUB: {
+          isSelected: false,
+          isStub: true,
+          keyword: '',
+          type: 'user',
+        },
       },
-    );
+    });
   };
 
-  const handleRemove = (id) => {
-    onMutate('delete', keywordItems[id]);
+  /**
+   * Adds item if the updated item is a stub. Otherwise forwards the update.
+   * @param item object Keyword item.
+   */
+  const handleUpdate = (item) => {
+    if (item.isStub) {
+      const { isSelected, keyword, type } = item;
+      onMutate('add', { isSelected, keyword, type });
+    } else {
+      onUpdate(item);
+    }
   };
+
+  /**
+   * Calls parent's onMutate() callback with flag to delete item. Deselects item to remove
+   *   it from the list of selected keywords.
+   * @param item
+   */
+  const handleRemove = (item) => {
+    onMutate('delete', { ...item, ...{ isSelected: false } });
+  };
+
+  React.useEffect(() => {
+    setSelfKeywordItems(keywordItems);
+  }, [keywordItems]);
 
   return (
     <div className={styles.wrapper}>
@@ -37,7 +67,11 @@ const UserList = (props) => {
       >
         <AddIcon />
       </ActionButton>
-      <KeywordList keywordItems={keywordItems} onRemove={handleRemove} onUpdate={onUpdate} />
+      <KeywordList
+        keywordItems={selfKeywordItems}
+        onRemove={handleRemove}
+        onUpdate={handleUpdate}
+      />
     </div>
   );
 };
@@ -45,7 +79,6 @@ const UserList = (props) => {
 UserList.propTypes = {
   keywordItems: PropTypes.objectOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
-    isSelected: PropTypes.bool.isRequired,
     keyword: PropTypes.string.isRequired,
   })).isRequired,
   onMutate: PropTypes.func.isRequired,
