ffe5d13c6320a6b03942ce732b459fb16a8a62ec
Kyle Benk <kjbenk@gmail.com>
2020-05-26 09:50:19 -0600

    Fic unit test

--- components/keywordPanel/keywordSelector/userKeywordList/index.spec.jsx
+++ components/keywordPanel/keywordSelector/userKeywordList/index.spec.jsx
@@ -4,24 +4,16 @@ import ActionButton from 'components/actionButton';
 import UserLKeywordList from './index';
 import KeywordList from '../keywordList';
 
-const mockKeywordItems = {
-  a: {
-    id: 'a',
-    isSelected: true,
-    keyword: 'keyword',
-    type: 'user',
-  },
-};
-
 describe('UserKeywordList', () => {
   it('Renders required child components', () => {
-    const wrapper = shallow(<UserLKeywordList
-      keywordItems={mockKeywordItems}
-      onMutate={jest.fn()}
-      onUpdate={() => true}
-    />);
+    jest.spyOn(React, 'useContext')
+      .mockImplementation(() => ({
+        dispatch: jest.fn(),
+        state: { userKeywords: ['keyword'] },
+      }));
+    const wrapper = shallow(<UserLKeywordList />);
 
-    expect(wrapper.find(KeywordList)).toHaveLength(Object.keys(mockKeywordItems).length);
+    expect(wrapper.find(KeywordList)).toHaveLength(1);
     expect(wrapper.find(ActionButton)).toHaveLength(1);
   });
 });
