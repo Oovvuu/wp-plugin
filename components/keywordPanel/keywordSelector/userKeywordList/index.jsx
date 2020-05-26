1cdc87918eb2bb4834081cb5d9cd26fb21b56b85
Kyle Benk <kjbenk@gmail.com>
2020-05-26 09:08:31 -0600

    Update user keywords state

--- components/keywordPanel/keywordSelector/userKeywordList/index.jsx
+++ components/keywordPanel/keywordSelector/userKeywordList/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import AddIcon from 'assets/add.svg';
 import ActionButton from 'components/actionButton';
+import oovvuuData from 'components/app/context';
 import KeywordList from '../keywordList';
 import styles from './userKeywordList.scss';
 
@@ -9,11 +9,14 @@ import styles from './userKeywordList.scss';
  * Manages adding and deleting user-defined keywords. Wraps the KeywordList component
  *   and passes through the onRemove() callback to enable editable functionality.
  */
-const UserList = (props) => {
-  const { keywordItems, onMutate, onUpdate } = props;
+const UserList = () => {
+  const {
+    dispatch,
+    state: { recommendedKeywords, userKeywords },
+  } = React.useContext(oovvuuData);
 
   // Need to keep a separate list to handle stubbing in a user-defined keyword.
-  const [selfKeywordItems, setSelfKeywordItems] = React.useState(keywordItems);
+  const [selfKeywordItems, setSelfKeywordItems] = React.useState({});
 
   /**
    * Stubs in a user-defined keyword item for input.
@@ -33,30 +36,53 @@ const UserList = (props) => {
   };
 
   /**
-   * Adds item if the updated item is a stub. Otherwise forwards the update.
-   * @param item object Keyword item.
-   */
-  const handleUpdate = (item) => {
-    if (item.isStub) {
-      const { isSelected, keyword, type } = item;
-      onMutate('add', { isSelected, keyword, type });
-    } else {
-      onUpdate(item);
-    }
-  };
-
-  /**
    * Calls parent's onMutate() callback with flag to delete item. Deselects item to remove
    *   it from the list of selected keywords.
    * @param item
    */
   const handleRemove = (item) => {
-    onMutate('delete', { ...item, ...{ isSelected: false } });
+    const { keyword } = item;
+    dispatch({ payload: userKeywords.filter((value) => value !== keyword), type: 'UPDATE_USER_KEYWORDS' });
+  };
+
+  /**
+   * Adds item if the updated item is a stub. Otherwise forwards the update.
+   * @param item object Keyword item.
+   */
+  const handleUpdate = (item) => {
+    const { keyword } = item;
+
+    // Do not add a user keyword if it is already a recommended keyword.
+    if (recommendedKeywords.includes(keyword)) {
+      handleRemove(item);
+      return;
+    }
+
+    dispatch({ payload: [...userKeywords, keyword], type: 'UPDATE_USER_KEYWORDS' });
+  };
+
+  /**
+   * Side effect to compile the master index of all keyword items, used for tracking
+   *   state throughout the selector tree.
+   *
+   * @param  {Array} selectionList An array list of the currently selected keyword strings.
+   */
+  const compileAllKeywordItems = (selectionList) => {
+    const indexedKeywords = userKeywords.reduce((carry, keyword) => ({
+      ...carry,
+      ...{
+        [keyword]: {
+          isSelected: selectionList.includes(keyword), keyword, type: 'user',
+        },
+      },
+    }), {});
+
+    setSelfKeywordItems({ ...indexedKeywords });
   };
 
   React.useEffect(() => {
-    setSelfKeywordItems(keywordItems);
-  }, [keywordItems]);
+    compileAllKeywordItems(userKeywords);
+  }, [userKeywords]);
 
   return (
     <div className={styles.wrapper}>
@@ -74,16 +100,6 @@ const UserList = (props) => {
       />
     </div>
   );
-};
-
-UserList.propTypes = {
-  keywordItems: PropTypes.objectOf(PropTypes.shape({
-    keyword: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    isSelected: PropTypes.bool.isRequired,
-  })).isRequired,
-  onMutate: PropTypes.func.isRequired,
-  onUpdate: PropTypes.func.isRequired,
 };
 
 export default UserList;
