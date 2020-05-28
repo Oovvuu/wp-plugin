0ea0b4db5b689c5775a0da034c58401b37ecaa4e
Jake Foster <jake@alley.co>
2020-05-28 11:30:42 -0400

    add getPositionKeys and update jsdoc

--- services/getHumanReadableEmptyReason.js
+++ services/getHumanReadableEmptyReason.js
@@ -1,8 +1,8 @@
 /**
  * Maps positionEmptyReason codes to human readable, FE-friendly values.
  *
- * @param  {[string]}  positionEmptyReason Position empty reason code from API response.
- * @returns {[string]} Human readable position empty reason.
+ * @param  {string}  positionEmptyReason Position empty reason code from API response.
+ * @returns {string} Human readable position empty reason.
  */
 const getHumanReadableEmptyReason = (positionEmptyReason) => {
   const { i18n: { __ } } = wp;
