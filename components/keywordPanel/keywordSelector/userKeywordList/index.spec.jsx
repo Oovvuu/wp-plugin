12e519ee2314609b4138873ebb9bc8b9e19ebbe3
Ryan Domingue <rychdom+github@gmail.com>
2020-06-03 15:48:05 -0700

    OVU-85 Fixes test

--- components/keywordPanel/keywordSelector/userKeywordList/index.spec.jsx
+++ components/keywordPanel/keywordSelector/userKeywordList/index.spec.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ActionButton from 'components/actionButton';
 import UserKeywordList from './index';
 import UserKeywordItem from './userKeywordItem';
 
 describe('UserKeywordList', () => {
-  // @todo fix it don't skip it.
-  it.skip('Renders required child components', () => {
+  it('Renders required child components', () => {
     jest.spyOn(React, 'useContext')
       .mockImplementation(() => ({
         dispatch: jest.fn(),
@@ -15,6 +13,5 @@ describe('UserKeywordList', () => {
     const wrapper = shallow(<UserKeywordList />);
 
     expect(wrapper.find(UserKeywordItem)).toHaveLength(1);
-    expect(wrapper.find(ActionButton)).toHaveLength(1);
   });
 });
