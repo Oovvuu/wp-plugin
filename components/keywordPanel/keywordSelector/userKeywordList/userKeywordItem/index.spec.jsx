a200ea825cf2c35d785f3ddfabfc7039c0989120
Ryan Domingue <rychdom+github@gmail.com>
2020-06-03 14:59:12 -0700

    OVU-85 Corrects test

--- components/keywordPanel/keywordSelector/userKeywordList/userKeywordItem/index.spec.jsx
+++ components/keywordPanel/keywordSelector/userKeywordList/userKeywordItem/index.spec.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import UserKeywordItem from './';
 
-const mockItem = { isSelected: false, keyword: 'keyword' };
-
 describe('UserKeywordItem', () => {
   it('Calls remove function when clicked', () => {
     const toRemove = jest.fn();
     const wrapper = shallow(<UserKeywordItem
-      item={mockItem}
+      keyword="keyword"
       onRemove={toRemove}
       onToggle={jest.fn()}
     />);
