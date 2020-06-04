f0cee1fac75d9f48a97623bb14a0357c86fc0e09
Julio Braña Bouza <julio@alley.co>
2020-06-04 22:26:38 +0300

    merge prod

--- services/insertEmbed.js
+++ services/insertEmbed.js
@@ -29,10 +29,18 @@ const insertEmbed = (id, videos) => {
       removeBlocks,
     } = dispatch('core/block-editor');
 
+    // Remove all un-needed data from videos.
+    const minimalVideos = videos.map((video) => ({
+      id: video.id,
+      preview: { ...video.preview },
+      thumbnail: { ...video.thumbnail },
+    }));
+
     // Create new embed block.
     const newBlock = createBlock(
       'oovvuu/embed', {
         id,
+        videos: JSON.stringify(minimalVideos),
       },
     );
 
@@ -46,41 +54,6 @@ const insertEmbed = (id, videos) => {
     // Insert block after 3rd paragraph.
     insertBlocks(newBlock, 3);
   }
-  const {
-    data: { dispatch, select },
-    blocks: { createBlock },
-  } = wp;
-
-  const {
-    getBlocks,
-  } = select('core/block-editor');
-
-  const {
-    insertBlocks,
-    removeBlocks,
-  } = dispatch('core/block-editor');
-
-  // Remove all un-needed data from videos.
-  const minimalVideos = videos.map((video) => ({
-    id: video.id,
-    preview: { ...video.preview },
-    thumbnail: { ...video.thumbnail },
-  }));
-
-  // Create new embed block.
-  const newBlock = createBlock(
-    'oovvuu/embed', {
-      id,
-      videos: JSON.stringify(minimalVideos),
-    },
-  );
-
-  const blocks = getBlocks();
-  const oovvuuEmbedBlocks = blocks.filter((value) => value.name === 'oovvuu/embed');
-  const clientIds = oovvuuEmbedBlocks.map((block) => block.clientId);
-
-  // Remove all current oovvuu embeds.
-  removeBlocks(clientIds);
 
   // Classic editor.
   if (tinymce?.editors?.content) {
