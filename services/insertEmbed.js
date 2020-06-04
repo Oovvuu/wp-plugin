1d9a77814c0b1b02010895686165c718da38f91d
Julio Braña Bouza <julio@alley.co>
2020-06-04 14:11:50 +0300

    kind of done

--- services/insertEmbed.js
+++ services/insertEmbed.js
@@ -1,3 +1,5 @@
+/* global tinymce */
+
 /**
  * Insert Oovvuu embed after 3rd paragraph
  *
@@ -9,37 +11,56 @@ const insertEmbed = (id) => {
     return false;
   }
 
-  const {
-    data: { dispatch, select },
-    blocks: { createBlock },
-  } = wp;
+  // Check if Gutenberg.
+  if (wp.data) {
+    const {
+      data: { dispatch, select },
+      blocks: { createBlock },
+    } = wp;
 
-  const {
-    getBlocks,
-  } = select('core/block-editor');
+    const {
+      getBlocks,
+    } = select('core/block-editor');
 
-  const {
-    insertBlocks,
-    removeBlocks,
-  } = dispatch('core/block-editor');
+    const {
+      insertBlocks,
+      removeBlocks,
+    } = dispatch('core/block-editor');
 
-  // Create new embed block.
-  const newBlock = createBlock(
-    'oovvuu/embed', {
-      id,
-    },
-  );
+    // Create new embed block.
+    const newBlock = createBlock(
+      'oovvuu/embed', {
+        id,
+      },
+    );
 
-  const blocks = getBlocks();
-  const oovvuuEmbedBlocks = blocks.filter((value) => value.name === 'oovvuu/embed');
-  const clientIds = oovvuuEmbedBlocks.map((block) => block.clientId);
+    const blocks = getBlocks();
+    const oovvuuEmbedBlocks = blocks.filter((value) => value.name === 'oovvuu/embed');
+    const clientIds = oovvuuEmbedBlocks.map((block) => block.clientId);
 
-  // Remove all current oovvuu embeds.
-  removeBlocks(clientIds);
+    // Remove all current oovvuu embeds.
+    removeBlocks(clientIds);
 
-  // Insert block after 3rd paragraph.
-  insertBlocks(newBlock, 3);
+    // Insert block after 3rd paragraph.
+    insertBlocks(newBlock, 3);
+  }
 
+  if (tinymce?.editors?.content) {
+    const content = tinymce.editors.content.getContent();
+
+    // Dummy element to process nodes.
+    const el = document.createElement('div');
+    el.innerHTML = content;
+
+    // Array of HTML elements.
+    const nodes = [...el.childNodes];
+    const nodesHtml = nodes.map((n) => n.outerHTML);
+    const filteredNodes = nodesHtml.filter((n) => !n.includes('blap'));
+    filteredNodes.splice(3, 0, `<p>[oovvuu-embed id=${id}]</p>`);
+    tinymce.editors.content.setContent(filteredNodes.join(''));
+  }
+
+  // Classic editor.
   return true;
 };
 export default insertEmbed;
