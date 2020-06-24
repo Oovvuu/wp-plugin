3eb3e1ec03aaca733716efa67ecdce958e0af164
Jake Foster <jake@alley.co>
2020-06-24 16:43:14 -0400

    add access to the oovvuu plugin sidebar if plugin is unpinned

--- components/app/index.jsx
+++ components/app/index.jsx
@@ -4,22 +4,30 @@ import OovvuuSmallSVGLogo from 'assets/oovvuu-small-logo.svg';
 import App from './app';
 import styles from './app.scss';
 
-const { PluginSidebar } = wp.editPost;
+const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
 const { registerPlugin } = wp.plugins;
+const label = 'Oovvuu';
 
 /**
  * Adds a new Document Settings Panel for all Oovvuu video embedding functionality.
  */
 const OovvuuPluginDocumentSettingPanel = () => (
-  <PluginSidebar
-    name="oovvuu"
-    title="Oovvuu"
-    className={styles.sidebar}
-    icon={OovvuuSmallSVGLogo}
-  >
-    <OovvuuDataProvider>
-      <App />
-    </OovvuuDataProvider>
-  </PluginSidebar>
+  <>
+    <PluginSidebarMoreMenuItem
+      target="oovvuu"
+    >
+      {label}
+    </PluginSidebarMoreMenuItem>
+    <PluginSidebar
+      name="oovvuu"
+      title="Oovvuu"
+      className={styles.sidebar}
+      icon={OovvuuSmallSVGLogo}
+    >
+      <OovvuuDataProvider>
+        <App />
+      </OovvuuDataProvider>
+    </PluginSidebar>
+  </>
 );
 registerPlugin('plugin-document-setting-panel-oovvuu', { render: OovvuuPluginDocumentSettingPanel });
