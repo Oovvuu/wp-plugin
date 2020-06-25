03d88ac63e1b3c9924903ef716d8994c0752a03c
Ryan Domingue <rychdom+github@gmail.com>
2020-06-25 11:49:40 -0700

    OVU-111 Adds search form event handlers

--- components/sidebar/search/index.jsx
+++ components/sidebar/search/index.jsx
@@ -9,13 +9,35 @@ import SearchIcon from 'assets/search.svg';
 const Search = (props) => {
   const { i18n: { __ } } = wp;
   const { onFormSubmission } = props;
+  const [searchValue, setSearchValue] = React.useState('');
 
+  /**
+   * Handles search form submission.
+   *
+   * @param event Event Event object.
+   */
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onFormSubmission([searchValue]);
+  };
+
+  /**
+   * Handles updating internal state for search input.
+   *
+   * @param event Event Event object.
+   */
+  const handleChange = (event) => {
+    const { target: { value } } = event;
+
+    setSearchValue(value);
+  };
 
   return (
     <form
       role="search"
       className={styles.form}
       autoComplete="off"
+      onSubmit={handleSubmit}
     >
       <label
         htmlFor="oovvuu-sidebar-search"
@@ -28,12 +50,14 @@ const Search = (props) => {
           id="oovvuu-sidebar-search"
           type="text"
           defaultValue=""
+          onChange={handleChange}
         />
       </label>
 
       <ActionButton
         type="submit"
         buttonStyle="icon"
+        onClickHandler={handleSubmit}
       >
         <SearchIcon />
         <span className="screen-reader-only">
