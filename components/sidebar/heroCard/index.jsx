013b3e96c8a3cbadf6511a8f0b753af3f1d06452
Robert Pratt <robert@alley.co>
2020-06-24 21:27:15 -0500

    Adds markup and styles.

--- components/sidebar/heroCard/index.jsx
+++ components/sidebar/heroCard/index.jsx
@@ -10,26 +10,34 @@ import styles from './heroCard.scss';
  */
 const HeroCardWrapper = (props) => {
   const { i18n: { __ } } = wp;
-  const { video: { id } } = props;
+  const { video: { thumbnail: { url }, title } } = props;
   const { dispatch } = React.useContext(OovvuuDataContext);
 
   return (
-    <div>
-      <h3>{__('Current Hero Position Video', 'oovvuu')}</h3>
-      {id}
-      <ActionButton
-        buttonStyle="icon"
-        className={styles.remove}
-        onClickHandler={() => dispatch({ type: 'REMOVE_SIDEBAR_SELECTED_HERO' })}
-      >
-        <ClearIcon />
-      </ActionButton>
+    <div className={styles.card}>
+      <div className={styles.header}>
+        <h3>{__('Current Hero Position Video', 'oovvuu')}</h3>
+        <ActionButton
+          buttonStyle="icon"
+          className={styles.remove}
+          onClickHandler={() => dispatch({ type: 'REMOVE_SIDEBAR_SELECTED_HERO' })}
+        >
+          <ClearIcon />
+        </ActionButton>
+      </div>
+      <div className={styles.content}>
+        <img src={url} alt="video thumbnail" draggable="false" />
+        <p>{title}</p>
+      </div>
     </div>
   );
 };
 
 HeroCardWrapper.propTypes = {
-  video: PropTypes.shape({ id: PropTypes.string.isRequired }).isRequired,
+  video: PropTypes.shape({
+    thumbnail: PropTypes.shape({ url: PropTypes.string.isRequired }),
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default HeroCardWrapper;
