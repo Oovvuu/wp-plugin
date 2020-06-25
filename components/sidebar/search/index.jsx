f2f63c28c67ad3f65a611b4a262b58b1ad38e372
Ryan Domingue <rychdom+github@gmail.com>
2020-06-25 11:42:02 -0700

    OVU-111 Updates search form markup

--- components/sidebar/search/index.jsx
+++ components/sidebar/search/index.jsx
@@ -1,20 +1,46 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
+import ActionButton from 'components/shared/actionButton';
+import SearchIcon from 'assets/search.svg';
 
 /**
  * Component for sidebar search.
  */
 const Search = (props) => {
+  const { i18n: { __ } } = wp;
   const { onFormSubmission } = props;
 
-  const inputSearchRef = useRef();
 
   return (
-    <>
-      <input type="text" defaultValue="" ref={inputSearchRef} />
-      <button type="button" onClick={() => onFormSubmission([inputSearchRef.current.value])}>Submit</button>
-    </>
+    <form
+      role="search"
+      className={styles.form}
+      autoComplete="off"
+    >
+      <label
+        htmlFor="oovvuu-sidebar-search"
+      >
+        <span className="screen-reader-only">
+          {__('Search Oovvuu', 'oovvuu')}
+        </span>
+
+        <input
+          id="oovvuu-sidebar-search"
+          type="text"
+          defaultValue=""
+        />
+      </label>
+
+      <ActionButton
+        type="submit"
+        buttonStyle="icon"
+      >
+        <SearchIcon />
+        <span className="screen-reader-only">
+          {__('Submit', 'oovvuu')}
+        </span>
+      </ActionButton>
+    </form>
   );
 };
 
