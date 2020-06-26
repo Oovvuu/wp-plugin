e503e2fde4e71f3c7167c0ecb46aed26be58504a
Kyle Benk <kjbenk@gmail.com>
2020-06-26 07:27:00 -0600

    Better check when latest videos sidebar should be shown

---
+++ services/recommendedVideosEmpty.js
@@ -0,0 +1,14 @@
+/**
+ * Determines if the recommended videos is empty.
+ *
+ * @param  {object} recommendedVideos The recommended videos obvject.
+ * @return {bool} True if recommended videos are empty, otherwise false.
+ */
+function recommendedVideosEmpty(recommendedVideos) {
+  return (
+    recommendedVideos?.hero.length > 0
+    && recommendedVideos?.positionTwo.length > 0
+  );
+}
+
+export default recommendedVideosEmpty;
