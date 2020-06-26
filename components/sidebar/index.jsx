8e0a6e2cfe8e5f4bbb3871ee01c5d61dbc6dba6c
Ryan Domingue <rychdom+github@gmail.com>
2020-06-26 15:56:24 -0700

    OVU-115 Corrects spacing in sidebar

--- components/sidebar/index.jsx
+++ components/sidebar/index.jsx
@@ -88,19 +88,17 @@ const SidebarWrapper = () => {
 
   const showLatestVideosWrapper = (
     <>
-      <section>
-        {
-          !isAddingVideo
-          && (isRemovingVideo || sidebarSelectedHeroVideo.id)
-          && (
-          <HeroCardWrapper
-            video={sidebarSelectedHeroVideo}
-            isRemovingVideo={isRemovingVideo}
-            updateIsRemovingVideo={setIsRemovingVideo}
-          />
-          )
-        }
-      </section>
+      {
+        !isAddingVideo
+        && (isRemovingVideo || sidebarSelectedHeroVideo.id)
+        && (
+        <HeroCardWrapper
+          video={sidebarSelectedHeroVideo}
+          isRemovingVideo={isRemovingVideo}
+          updateIsRemovingVideo={setIsRemovingVideo}
+        />
+        )
+      }
 
       <section>
         <Search onFormSubmission={(keywords) => { handleFetchLatestVideos(keywords); }} />
