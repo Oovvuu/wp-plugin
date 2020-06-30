4febe6922953225889c66e06bbf3dadf823d8811
Geoff Tsai <geoff.tsai@oovvuu.com>
2020-06-30 11:53:42 +1000

    mysql-admin

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,7 +9,7 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 sudo apt-get update && sudo apt-get install gcc-8-base subversion 
 sudo docker-php-ext-install mysqli
 sudo sh -c "printf '\ndeb http://ftp.us.debian.org/debian sid main\n' >> /etc/apt/sources.list"
-#sudo apt-get update && sudo apt-get install mysql-client-5.7
+sudo apt-get update && sudo apt-get install mysql-client-5.7
 # sudo apt-get install subversion
 # sudo apt-get install libapache2-mod-svn
 
