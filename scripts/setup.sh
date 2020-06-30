f51929b98f782c2b28649478c7f042275d568b11
Geoff Tsai <geoff.tsai@oovvuu.com>
2020-06-30 10:12:30 +1000

    updte for debian install

---
+++ scripts/setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -e
+
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
+
+# debian install svn
+apt-get install subversion
+apt-get install libapache2-mod-svn
+
+
+# install phpunit
+composer global require "phpunit/phpunit=6.1.*"
+composer global require automattic/vipwpcs
+
+
+# Set up the WordPress installation.
+bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION
