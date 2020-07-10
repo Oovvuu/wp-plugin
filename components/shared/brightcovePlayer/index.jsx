a9eba542e44309fecd3557907868d542d7c5227c
Robert Pratt <robert@alley.co>
2020-07-10 06:30:27 -0500

    Adds event emitter and listener for sync'ing playback of all instances.

--- components/shared/brightcovePlayer/index.jsx
+++ components/shared/brightcovePlayer/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import eventBus from 'services/eventBus';
 import ReactPlayerLoader from '@brightcove/react-player-loader';
 
 /**
@@ -7,6 +8,34 @@ import ReactPlayerLoader from '@brightcove/react-player-loader';
  */
 const BrightcovePlayer = (props) => {
   const { videoId, playerId, accountId } = props;
+  let player = React.useRef(null);
+
+  /**
+   * Handler for succes method of loader. Stores ref to player instance and sets listener.
+   * @param loaded
+   */
+  const handleSuccess = (loaded) => {
+    player = loaded.ref;
+
+    // eslint-disable-next-line no-underscore-dangle
+    player.on('play', () => { eventBus.dispatch('oovvuuPlayVideo', { videoId: player.id_ }); });
+  };
+
+  /**
+   * Sets event bus listener for sync'ing playing state of instance.
+   */
+  React.useEffect(() => {
+    const callback = (details) => {
+      // eslint-disable-next-line no-underscore-dangle
+      if (player.id_ !== details.videoId) {
+        player.pause();
+      }
+    };
+
+    eventBus.on('oovvuuPlayVideo', callback);
+
+    return () => { eventBus.remove('oovvuuPlayVideo', callback); };
+  }, []);
 
   return (
     <ReactPlayerLoader
@@ -14,6 +43,7 @@ const BrightcovePlayer = (props) => {
       playerId={playerId}
       videoId={videoId}
       embedOptions={{ responsive: true }}
+      onSuccess={handleSuccess}
     />
   );
 };
