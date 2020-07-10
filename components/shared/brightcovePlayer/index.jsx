70b9128975de3780d4d930618f24f395c81d74f8
Robert Pratt <robert@alley.co>
2020-07-10 08:39:11 -0500

    OVU-150: Video Playback Toggling (#13)
    * Adds event emitter and listener for sync'ing playback of all instances.
    
    * Adds event emitter on modal toggle.
    
    * Adds listener for pause event. Adds guard for existance of player.
    
    * Resolve merge conflict.

--- components/shared/brightcovePlayer/index.jsx
+++ components/shared/brightcovePlayer/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import eventBus from 'services/eventBus';
 import ReactPlayerLoader from '@brightcove/react-player-loader';
 
 /**
@@ -7,6 +8,41 @@ import ReactPlayerLoader from '@brightcove/react-player-loader';
  */
 const BrightcovePlayer = (props) => {
   const { videoId, playerId, accountId } = props;
+  let player = React.useRef(null);
+
+  /**
+   * Handler for success method of loader. Stores ref to player instance and sets listener.
+   * @param loaded
+   */
+  const handleSuccess = (loaded) => {
+    player = loaded.ref;
+
+    // eslint-disable-next-line no-underscore-dangle
+    player.on('play', () => { eventBus.dispatch('oovvuuPlayVideo', { videoId: player.id_ }); });
+  };
+
+  /**
+   * Sets event bus listeners for sync'ing playing state of instance.
+   */
+  React.useEffect(() => {
+    const handlePauseVideo = () => {
+      player.pause();
+    };
+    const handlePlayVideo = (details) => {
+      // eslint-disable-next-line no-underscore-dangle
+      if (!!player && player.id_ !== details.videoId) {
+        player.pause();
+      }
+    };
+
+    eventBus.on('oovvuuPlayVideo', handlePlayVideo);
+    eventBus.on('oovvuuPauseVideo', handlePauseVideo);
+
+    return () => {
+      eventBus.remove('oovvuuPauseVideo', handlePauseVideo);
+      eventBus.remove('oovvuuPlayVideo', handlePlayVideo);
+    };
+  }, []);
 
   return (
     <ReactPlayerLoader
@@ -14,6 +50,7 @@ const BrightcovePlayer = (props) => {
       playerId={playerId}
       videoId={videoId}
       embedOptions={{ responsive: true }}
+      onSuccess={handleSuccess}
     />
   );
 };
