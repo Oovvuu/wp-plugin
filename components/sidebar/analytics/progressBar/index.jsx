629b4fbbd33f13291e978735880bd1ae0e6cda42
Renato Alves <renato@alley.co>
2020-07-30 21:27:54 -0300

    Rounding the progress bar width value

--- components/sidebar/analytics/progressBar/index.jsx
+++ components/sidebar/analytics/progressBar/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './progressBar.scss';
 
 const ProgressBar = ({ width, percent }) => {
-  const value = percent * width;
+  const value = Math.round(percent * width);
   return (
     <div>
       <div className={styles.parent} style={{ width }}>
@@ -14,8 +14,8 @@ const ProgressBar = ({ width, percent }) => {
 };
 
 ProgressBar.propTypes = {
-  width: PropTypes.string.isRequired,
-  percent: PropTypes.string.isRequired,
+  width: PropTypes.number.isRequired,
+  percent: PropTypes.number.isRequired,
 };
 
 export default ProgressBar;
