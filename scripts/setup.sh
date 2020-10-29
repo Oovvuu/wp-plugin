f95621ec2ea75a6f03c2d0643fb336d5333792b4
Geoff Tsai <geoff.tsai@oovvuu.com>
2020-10-30 10:40:33 +1100

    add in the variable analysis to composer

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -14,6 +14,7 @@ sudo apt-get update && sudo apt-get install default-mysql-client
 # install phpunit
 composer global require "phpunit/phpunit=6.1.*"
 composer global require automattic/vipwpcs
+composer require --dev sirbrillig/phpcs-variable-analysis
 
 WP_VERSION="latest"
 # Set up the WordPress installation.
